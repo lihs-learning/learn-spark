ba48715a144c6b70e22f238a47082437b0ef2513
LiHS <lihsai0@gmail.com>
2021-02-18 13:13:26 +0800

    feat: word count save to file

--- 04-rdd-operation/11-wc.py
+++ 04-rdd-operation/11-wc.py
@@ -3,25 +3,23 @@ import sys
 from pyspark import SparkConf, SparkContext
 
 
-def run_wc(sc: SparkContext, fp: str):
-    result = sc.textFile(fp) \
+def run_wc(sc: SparkContext, ifp: str, ofp: str):
+    sc.textFile(ifp) \
         .flatMap(lambda line: line.split('\t')) \
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda m, n: m + n) \
-        .collect()
-
-    for (word, count) in result:
-        print('%s: %i' % (word, count))
+        .saveAsTextFile(ofp)
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 2:
-        print("Usage: 11-wc <input>", file=sys.stderr)
+    if len(sys.argv) < 3:
+        print("Usage: 11-wc <input> <output>", file=sys.stderr)
         sys.exit(-1)
 
-    file_path = sys.argv[1]
+    in_file_path = sys.argv[1]
+    out_file_path = sys.argv[2]
 
     spark_conf = SparkConf()
     with SparkContext(conf=spark_conf) as spark_context:
-        run_wc(spark_context, file_path)
+        run_wc(spark_context, in_file_path, out_file_path)
--- 04-rdd-operation/11-wc.sh
+++ 04-rdd-operation/11-wc.sh
@@ -2,5 +2,12 @@
 
 cp ../data/words.txt /tmp/spark-test-words.txt
 # 也支持目录 通配符*
-$SPARK_HOME/bin/spark-submit --master local[2] --name 0411 ./11-wc.py file:///tmp/spark-test-words.txt
+$SPARK_HOME/bin/spark-submit --master local[2] --name 0411 ./11-wc.py file:///tmp/spark-test-words.txt file:///tmp/wc-result
 rm /tmp/spark-test-words.txt
+
+if [ -f /tmp/wc-result/_SUCCESS ]; then
+    cat /tmp/wc-result/part-*
+    rm -fr /tmp/wc-result
+else
+    echo "something is errored"
+fi
